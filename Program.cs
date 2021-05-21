4285e051b96e00966d94801b45a5cd17e25b31c7
Adriano Arruda <belmirioadrianoarruda@gmail.com>
2021-05-20 22:35:40 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace wello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int dia, mes, ano, idade;
+            
+            //Console.clear();
+            Console.WriteLine("nome");
+            nome = Console.ReadLine();
+            Console.WriteLine("ola " + nome);
+
+            Console.WriteLine("dia");
+            dia = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("mes");
+            mes = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("ano");
+            ano = Convert.ToInt16(Console.ReadLine());
+            idade = 2021 - ano;
+            Console.WriteLine("ola " + nome + " voce tem " + idade + " anos");
+
+            if (true);
+            {
+               if (idade >= 18);
+            }
+
+            {
+              Console.WriteLine(nome + " voce é maoir de idade");
+              //idade = Console.ReadLine();
+            }
+
+            else if (false);
+            {
+              Console.WriteLine(nome + " voce é menor de idade");  
+            }
+               //idade = Console.ReadLine();
+        }
+    }
+}
